ebae01fe4b2bae9acac03d9b79af2028be30fecd
Maschinist-LZY <modupriest@gmail.com>
2021-09-14 17:44:28 +0100

    Q41

---
+++ Q41.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        int num = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]<=0){
+                nums[i] = n+1;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            num = abs(nums[i]);
+            if(num <= n)
+            {
+                nums[num - 1] = -1*abs(nums[num - 1]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>0)
+            {
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+};
